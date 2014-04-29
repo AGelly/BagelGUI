ccd9a913087f5dc4369304b13ae648e25b6542ea
Andrew Gelstein <Gelstein.andrew@gmail.com>
2014-04-29 10:58:51 -0500

    Create GreetingPanel.java

---
+++ GreetingPanel.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class GreetingPanel extends JPanel
+{
+	public GreetingPanel()
+	{
+		JLabel greeting = new JLabel("Welcome to Brandi's Bagel House");
+		
+		add(greeting);
+	}
+}
